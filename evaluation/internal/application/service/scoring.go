0bc009415b49c9c3d000d5ab2b52c358dbfe222f
aakashloyar <aakashloyar@gmail.com>
2026-09-06 21:50:32 +0530

    [evaluation] update scoring service

--- evaluation/internal/application/service/scoring.go
+++ evaluation/internal/application/service/scoring.go
@@ -2,14 +2,13 @@ package evaluation
 
 import "github.com/aakashloyar/elevate/evaluation/internal/domain"
 
-
 func EvaluateAnswer(problem domain.Problem, selectedOptions []string, scheme domain.MarkingScheme) domain.QuestionResult {
 	result := domain.QuestionResult{
 		ProblemID:       problem.ID,
-		Type: 		     problem.Type,
+		Type:            problem.Type,
 		SelectedOptions: evaluateOptions(problem, selectedOptions),
 	}
-	updateProblemResult(problem,result, scheme)
+	updateProblemResult(problem, &result, scheme)
 	return result
 }
 
@@ -19,46 +18,46 @@ func evaluateOptions(problem domain.Problem, selectedOptions []string) []domain.
 	}
 	options := problem.Options
 	if problem.Type == domain.ProblemTypeNumerical {
-		return []domain.SelectedOption {
-			{
-				ID: "",
-				Text: selectedOptions[0],
-				IsCorrect: options[0].Text == selectedOptions[0],
-			},
-		}
-	} else if problem.Type == domain.ProblemTypeSingle {
 		isCorrect := false
-		for _, option := range options {
-			if option.ID == selectedOptions[0] {
-				isCorrect = option.IsCorrect
-				break
-			}
+		if len(options) > 0 {
+			isCorrect = options[0].Text == selectedOptions[0]
 		}
 		return []domain.SelectedOption{
 			{
-				ID:        selectedOptions[0],
-				Text:      "",
+				Text:      selectedOptions[0],
 				IsCorrect: isCorrect,
 			},
 		}
-	} else{
+	} else if problem.Type == domain.ProblemTypeSingle {
+		selectedOption := domain.SelectedOption{
+			ID: selectedOptions[0],
+		}
+		for _, option := range options {
+			if option.ID == selectedOptions[0] {
+				selectedOption.Text = option.Text
+				selectedOption.IsCorrect = option.IsCorrect
+				break
+			}
+		}
+		return []domain.SelectedOption{selectedOption}
+	} else {
 		res := make([]domain.SelectedOption, 0, len(selectedOptions))
-		for _, option := range selectedOptions {
+		for _, selectedOptionID := range selectedOptions {
+			selectedOption := domain.SelectedOption{ID: selectedOptionID}
 			for _, opt := range options {
-				if opt.ID == option {
-					res = append(res, domain.SelectedOption{
-						ID:        opt.ID,
-						Text:      opt.Text,
-						IsCorrect: opt.IsCorrect,
-					})
+				if opt.ID == selectedOptionID {
+					selectedOption.Text = opt.Text
+					selectedOption.IsCorrect = opt.IsCorrect
+					break
 				}
 			}
+			res = append(res, selectedOption)
 		}
 		return res
 	}
 }
 
-func updateProblemResult(problem domain.Problem, result domain.QuestionResult, scheme domain.MarkingScheme) {
+func updateProblemResult(problem domain.Problem, result *domain.QuestionResult, scheme domain.MarkingScheme) {
 	correct, incorrect, skipped := marks(result.Type, scheme)
 	if problem.Type != domain.ProblemTypeMultiple {
 		if len(result.SelectedOptions) == 0 {
@@ -76,6 +75,12 @@ func updateProblemResult(problem domain.Problem, result domain.QuestionResult, s
 			result.Status = domain.QuestionResultStatusSkipped
 			result.Marks = skipped
 		} else {
+			if hasDuplicateSelectedOptions(result.SelectedOptions) {
+				result.Status = domain.QuestionResultStatusIncorrect
+				result.Marks = incorrect
+				return
+			}
+
 			selectedCorrectOptionCount := 0
 			for _, option := range result.SelectedOptions {
 				if !option.IsCorrect {
@@ -94,9 +99,12 @@ func updateProblemResult(problem domain.Problem, result domain.QuestionResult, s
 			if selectedCorrectOptionCount == totalCorrectOptionCount {
 				result.Status = domain.QuestionResultStatusCorrect
 				result.Marks = correct
-			} else {
+			} else if totalCorrectOptionCount > 0 {
 				result.Status = domain.QuestionResultStatusPartiallyCorrect
 				result.Marks = float64(selectedCorrectOptionCount) / float64(totalCorrectOptionCount) * correct
+			} else {
+				result.Status = domain.QuestionResultStatusIncorrect
+				result.Marks = incorrect
 			}
 		}
 	}
@@ -113,4 +121,15 @@ func marks(kind domain.ProblemType, s domain.MarkingScheme) (float64, float64, f
 	default:
 		return 0, 0, 0
 	}
+}
+
+func hasDuplicateSelectedOptions(options []domain.SelectedOption) bool {
+	seen := make(map[string]struct{}, len(options))
+	for _, option := range options {
+		if _, ok := seen[option.ID]; ok {
+			return true
+		}
+		seen[option.ID] = struct{}{}
+	}
+	return false
 }
