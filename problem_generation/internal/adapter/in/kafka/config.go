082523a467fd37c24b26f24acae6426e738415b5
aakashloyar <aakashloyar@gmail.com>
2026-09-07 12:30:49 +0530

    [problem_generation] add worker consumer

---
+++ problem_generation/internal/adapter/in/kafka/config.go
@@ -0,0 +1,47 @@
+package kafka
+
+import (
+	"crypto/tls"
+
+	in "github.com/aakashloyar/elevate/problem_generation/internal/application/ports/in"
+	"github.com/twmb/franz-go/pkg/kgo"
+	"github.com/twmb/franz-go/pkg/sasl/plain"
+)
+
+type Config struct {
+	Brokers   []string
+	Topics    []string
+	ClientID  string
+	GroupID   string
+	APIKey    string
+	APISecret string
+}
+
+type Consumer struct {
+	kafkaClient *KafkaConsumerClient
+	service     in.ProcessGenerationJobService
+}
+
+type KafkaConsumerClient struct {
+	client *kgo.Client
+	topics []string
+}
+
+func (cfg Config) NewConsumer(service in.ProcessGenerationJobService) (*Consumer, error) {
+	client, err := kgo.NewClient(
+		kgo.SeedBrokers(cfg.Brokers...),
+		kgo.ConsumerGroup(cfg.GroupID),
+		kgo.ConsumeTopics(cfg.Topics...),
+		kgo.ClientID(cfg.ClientID),
+		kgo.DialTLSConfig(&tls.Config{}),
+		kgo.SASL(plain.Auth{User: cfg.APIKey, Pass: cfg.APISecret}.AsMechanism()),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Consumer{
+		kafkaClient: &KafkaConsumerClient{client: client, topics: cfg.Topics},
+		service:     service,
+	}, nil
+}
---
+++ problem_generation/internal/adapter/in/kafka/consumer.go
@@ -0,0 +1,47 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aakashloyar/elevate/problem_generation/internal/application/ports/out"
+)
+
+func (c *Consumer) Start(ctx context.Context) error {
+	for ctx.Err() == nil {
+		fetches := c.kafkaClient.client.PollFetches(ctx)
+		if errs := fetches.Errors(); len(errs) > 0 {
+			for _, err := range errs {
+				log.Printf("problem generation kafka fetch error: %v", err)
+			}
+			continue
+		}
+
+		iter := fetches.RecordIter()
+		for !iter.Done() {
+			record := iter.Next()
+			var event out.GenerationRequestedEvent
+			if err := json.Unmarshal(record.Value, &event); err != nil {
+				log.Printf("invalid generation-requested event: %v", err)
+				if commitErr := c.kafkaClient.client.CommitRecords(ctx, record); commitErr != nil {
+					log.Printf("commit rejected generation event failed: %v", commitErr)
+				}
+				continue
+			}
+
+			if err := c.service.Execute(ctx, event); err != nil {
+				log.Printf("generation job failed for job %s: %v", event.JobID, err)
+				continue
+			}
+			if err := c.kafkaClient.client.CommitRecords(ctx, record); err != nil {
+				log.Printf("commit generation event: %v", err)
+			}
+		}
+	}
+	return ctx.Err()
+}
+
+func (c *Consumer) Close() {
+	c.kafkaClient.client.Close()
+}
